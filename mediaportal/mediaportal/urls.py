4f370f4b97a679f57362512f96bc4279d4db6d0e
varnikgen <1Nhfycbkmdfybz!>
2019-10-08 13:50:33 +1000

    add test commit

--- mediaportal/mediaportal/urls.py
+++ mediaportal/mediaportal/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+# тест
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portalapp.urls'))
