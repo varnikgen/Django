1436d8669d84463e8fc5824a6ce188f21e9cbc4d
varnikgen <1Nhfycbkmdfybz!>
2019-10-09 14:48:52 +1000

    create model Post, add template Index, Detail

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def posts_list(request):
-    n = ['Олег', 'Маша', 'Оля', 'Ксюша']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post': post})
