f022360259c4c4bcfaeff1cb79475b266278fa11
varnikgen <1Nhfycbkmdfybz!>
2019-10-10 13:32:27 +1000

    Add class Based View(PostDetail, TagDetail), add utils.py with Mixin

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.views.generic import View
+
 from .models import Post, Tag
+from .utils import ObjectsDetailMixin
 
 
 # Create your views here.
@@ -9,16 +12,16 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectsDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectsDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
