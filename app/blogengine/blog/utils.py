cc75b037186780c5cfeda4e992df15f15dfaec87
varnikgen <1Nhfycbkmdfybz!>
2019-10-10 13:41:03 +1000

    Cleaning

--- app/blogengine/blog/utils.py
+++ app/blogengine/blog/utils.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
-from .models import *
-
 
 class ObjectsDetailMixin:
     model = None
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import get_object_or_404
 from django.views.generic import View
 
 from .models import Post, Tag
 from .utils import ObjectsDetailMixin
 
 
-# Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
